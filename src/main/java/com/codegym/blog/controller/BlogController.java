6735cfb745859fe62b9df72c678851154223f03a
hoang4963 <hoangth.finbox@gmail.com>
2022-10-27 14:34:26 +0700

    a

--- src/main/java/com/codegym/blog/controller/BlogController.java
+++ src/main/java/com/codegym/blog/controller/BlogController.java
@@ -5,6 +5,8 @@ import com.codegym.blog.service.blog.IBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,82 +14,53 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
+@RequestMapping("/api/blogs")
 public class BlogController {
 
     @Autowired
     private IBlogService blogService;
 
-    @GetMapping("/create-blog")
-    public ModelAndView showCreateForm() {
-        ModelAndView modelAndView = new ModelAndView("/blog/create");
-        modelAndView.addObject("blog", new Blog());
-        return modelAndView;
-    }
-
-    @PostMapping("/create-blog")
-    public ModelAndView saveBlog(@ModelAttribute("blog") Blog blog) {
-        blogService.save(blog);
-        ModelAndView modelAndView = new ModelAndView("/blog/create");
-        modelAndView.addObject("blog", new Blog());
-        modelAndView.addObject("message", "New blog created successfully");
-        return modelAndView;
-    }
-
-    @GetMapping("/blogs")
-    public ModelAndView listBlogs(@RequestParam("search") Optional<String> search, Pageable pageable){
-        Page<Blog> blogs;
-        if(search.isPresent()){
-            blogs = blogService.findAllByNameContaining(search.get(), pageable);
-        } else {
-            blogs = blogService.findAll(pageable);
+    @GetMapping
+    public ResponseEntity<Iterable<Blog>> findAllBlog() {
+        List<Blog> blogs = (List<Blog>) blogService.findAll();
+        if (blogs.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
-        ModelAndView modelAndView = new ModelAndView("/blog/list");
-        modelAndView.addObject("blogs", blogs);
-        return modelAndView;
+        return new ResponseEntity<>(blogs, HttpStatus.OK);
     }
 
-
-    @GetMapping("/edit-blog/{id}")
-    public ModelAndView showEditForm(@PathVariable Long id) {
-        Optional<Blog> blog = blogService.findById(id);
-        if (blog.isPresent()) {
-            ModelAndView modelAndView = new ModelAndView("/blog/edit");
-            modelAndView.addObject("blog", blog.get());
-            return modelAndView;
-        } else {
-            ModelAndView modelAndView = new ModelAndView("/error.404");
-            return modelAndView;
+    @GetMapping("/{id}")
+    public ResponseEntity<Blog> findBlogById(@PathVariable Long id) {
+        Optional<Blog> blogOptional = blogService.findById(id);
+        if (!blogOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<>(blogOptional.get(), HttpStatus.OK);
     }
 
-    @PostMapping("/edit-blog")
-    public ModelAndView updateBlog(@ModelAttribute("blog") Blog blog) {
-        blogService.save(blog);
-        ModelAndView modelAndView = new ModelAndView("/blog/edit");
-        modelAndView.addObject("blog", blog);
-        modelAndView.addObject("message", "Blog updated successfully");
-        return modelAndView;
+    @PostMapping
+    public ResponseEntity<Blog> saveBlog(@RequestBody Blog blog) {
+        return new ResponseEntity<>(blogService.save(blog), HttpStatus.CREATED);
     }
 
-    @GetMapping("/delete-blog/{id}")
-    public ModelAndView showDeleteForm(@PathVariable Long id) {
-        Optional<Blog> blog = blogService.findById(id);
-        if (blog.isPresent()) {
-            ModelAndView modelAndView = new ModelAndView("/blog/delete");
-            modelAndView.addObject("blog", blog.get());
-            return modelAndView;
-
-        } else {
-            ModelAndView modelAndView = new ModelAndView("/error.404");
-            return modelAndView;
+    @PutMapping("/{id}")
+    public ResponseEntity<Blog> updateBlog(@PathVariable Long id, @RequestBody Blog blog) {
+        Optional<Blog> blogOptional = blogService.findById(id);
+        if (!blogOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        blog.setId(blogOptional.get().getId());
+        return new ResponseEntity<>(blogService.save(blog), HttpStatus.OK);
     }
 
-    @PostMapping("/delete-blog")
-    public String deleteBlog(@ModelAttribute("blog") Blog blog) {
-        blogService.remove(blog.getId());
-        return "redirect:blogs";
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Blog> deleteBlog(@PathVariable Long id) {
+        Optional<Blog> blogOptional = blogService.findById(id);
+        if (!blogOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        blogService.remove(id);
+        return new ResponseEntity<>(blogOptional.get(), HttpStatus.NO_CONTENT);
     }
-
 }
